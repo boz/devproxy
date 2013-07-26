30adbf419e5e6856f534af964d71a24b1c66a764
Adam Bozanich <adam.boz@gmail.com>
2013-07-26 11:18:42 -0700

    Teardown session on connection refused

--- lib/devproxy/connection.rb
+++ lib/devproxy/connection.rb
@@ -1,32 +1,25 @@
-require 'net/ssh'
-require 'devproxy/net-ssh-patch'
+require 'devproxy/session'
 
 module Devproxy
-  class Connection
-    class Error                 < StandardError; end
-    class Error::Authentication < Error        ; end
-    HEARTBEAT = "HEARTBEAT"
+  class Connection < Struct.new(:options)
+    MAX_DOTS  = 60
 
-    attr_reader :options, :ssh
-
-    MAX_DOTS = 60
-
-    def initialize options, ssh
-      @options, @ssh, @halt = options, ssh, false
+    def initialize options
+      super
       reset_dots!
     end
 
     def loop!
-      @ssh.loop { !halt? }
+      @session.shutdown! if @session
+      @session = Session.create(options,self)
+      @session.loop!
     rescue Errno::ECONNREFUSED
       on_stderr "CONNECTION REFUSED.  Is there anything listening on port #{options.port}?"
       retry
     end
+
     def stop!
-      @halt = true
-    end
-    def halt?
-      @halt
+      @session.stop! if @session
     end
 
     def on_connected
@@ -61,39 +54,11 @@ module Devproxy
     def reset_dots!
       @dotno = 0
     end
-
     def self.loop!(options)
       create(options).loop!
     end
-
     def self.create(options)
-      ssh        = open_ssh(options)
-      connection = new(options,ssh)
-      ssh.forward.remote(options.port,options.listen,0,'0.0.0.0')
-      channel    = ssh.exec(options.proxy) do |ch,stream,data|
-        if stream == :stdout
-          if data.start_with?(HEARTBEAT)
-            connection.on_heartbeat(data)
-          else
-            connection.on_stdout(data)
-          end
-        else
-          connection.on_stderr(data)
-        end
-      end
-      channel.on_close do
-        connection.on_close
-      end
-      connection.on_connected
-      connection
-    rescue Net::SSH::AuthenticationFailed
-      raise Error::Authentication, "Authentication Failed: Invalid username or SSH key"
-    end
-    def self.open_ssh(options)
-      Net::SSH.start(options.host, options.username,{
-        :port => options.remote_port,
-        :user_known_hosts_file => "/dev/null",
-      })
+      new(options)
     end
   end
 end
---
+++ lib/devproxy/session.rb
@@ -0,0 +1,52 @@
+require 'net/ssh'
+require 'devproxy/net-ssh-patch'
+
+module Devproxy
+  class Session < Struct.new(:ssh)
+    class Error                 < StandardError; end
+    class Error::Authentication < Error        ; end
+
+    HEARTBEAT = "HEARTBEAT"
+
+    def shutdown!
+      ssh.shutdown!
+    end
+
+    def loop!
+      ssh.loop { !@halt }
+    end
+
+    def stop!
+      @halt = true
+    end
+
+    def self.create(options,connection)
+      ssh        = open_ssh(options)
+      ssh.forward.remote(options.port,options.listen,0,'0.0.0.0')
+      channel    = ssh.exec(options.proxy) do |ch,stream,data|
+        if stream == :stdout
+          if data.start_with?(HEARTBEAT)
+            connection.on_heartbeat(data)
+          else
+            connection.on_stdout(data)
+          end
+        else
+          connection.on_stderr(data)
+        end
+      end
+      channel.on_close do
+        connection.on_close
+      end
+      connection.on_connected
+      new(ssh)
+    rescue Net::SSH::AuthenticationFailed
+      raise Error::Authentication, "Authentication Failed: Invalid username or SSH key"
+    end
+    def self.open_ssh(options)
+      Net::SSH.start(options.host, options.username,{
+        :port => options.remote_port,
+        :user_known_hosts_file => "/dev/null",
+      })
+    end
+  end
+end
